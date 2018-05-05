e540882dde46588c9c142942417bc1d52d1b805b
Roger Fos Soler <desaroger23@gmail.com>
2018-05-05 19:26:36 +0200

    Temporary change for build script

--- bin/build.sh
+++ bin/build.sh
@@ -2,5 +2,6 @@
 set -e
 
 cd laradock-symodo
-docker-compose build --no-cache workspace nginx mongo
+docker-compose build workspace nginx mongo
+#docker-compose build --no-cache workspace nginx mongo
 cd ..
